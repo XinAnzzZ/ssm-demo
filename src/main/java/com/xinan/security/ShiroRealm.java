7cd6477af2d25535f99ff3426997073e2be2c1ef
XinAnzzZ <1150989249@qq.com>
2018-08-12 22:11:04 +0800

    shiro

---
+++ src/main/java/com/xinan/security/ShiroRealm.java
@@ -0,0 +1,24 @@
+package com.xinan.security;
+
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.AuthenticationInfo;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.realm.Realm;
+
+/**
+ * @author XinAnzzZ
+ * @date 2018/8/12 0012 13:14
+ */
+public class ShiroRealm implements Realm {
+    public String getName() {
+        return null;
+    }
+
+    public boolean supports(AuthenticationToken authenticationToken) {
+        return false;
+    }
+
+    public AuthenticationInfo getAuthenticationInfo(AuthenticationToken authenticationToken) throws AuthenticationException {
+        return null;
+    }
+}
